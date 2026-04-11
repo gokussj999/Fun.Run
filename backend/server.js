66ae503b954ddf34d0d40b65cd1157c777261749
FunRun <tumhara_github_email_yahan>
2026-04-11 07:25:42 +0500

    debug incoming requests

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,11 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
+  console.log("🌍 incoming:", req.method, req.url);
+  next();
+});
+
+app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
